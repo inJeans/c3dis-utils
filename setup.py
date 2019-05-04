03a8ee8861cb5f4f1258170504a1f12e43d555f3
Chris Watkins (macbook) <christopher.watkins@me.com>
2019-05-04 21:56:23 +1000

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="utils",
+    version="0.0.1",
+    author="Christoper Watkins",
+    author_email="chris.watkins@csiro.au",
+    description="Utility functions for the C3DIS ML Workshop",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/inJeans/c3dis-utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
